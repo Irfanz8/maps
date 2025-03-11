2c556177dc24d7a24c2543915ae979c0eca0a55c
Irfan aprilyana <irfanaprilyana08@gmail.com>
2025-03-11 11:43:04 +0700

    build:export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'docs', // GitHub Pages hanya membaca dari folder 'docs'
+  images: {
+    unoptimized: true, // Diperlukan jika menggunakan Next.js Image
+  },
 };
 
 export default nextConfig;
